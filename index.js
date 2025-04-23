c683022c9c840771695bf061084f7ac46cc272bc
FlavioUser222 <flavinhoth12@gmail.com>
2025-04-23 15:16:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,37 +3,70 @@ import sqlite3 from "sqlite3"
 import cors from "cors"
 import bodyParser from "body-parser"
 
-
-
-
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded())
 app.use(cors())
 
+
 const db = new sqlite3.Database("./db.sqlite")
 
+db.serialize(() => {
+    db.run(`CREATE TABLE IF NOT EXISTS jogos(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome varchar(100),
+        img TEXT,
+        valor FLOAT,
+        descricao TEXT,
+        classificacao TEXT
+    );
 
-db.serialize(()=>{
-   db.run(`CREATE TABLE IF NOT EXISTS tabela(
-        id PRIMARY KEY AUTOINCREMENT,
-        nome varchar(100)
-    )`)
+        CREATE TABLE IF NOT EXISTS login(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome varchar(100),
+        email varchar(100),
+        senha varchar(30)
+    )
+    
+    `)
 })
 
 
-app.get("/home",(req,res)=>{
-   db.all(`SELECT * FROM tabela `,[],(err,rows)=>{
-     res.json(rows)
-   })
+app.get("/home", (req, res) => {
+   
 })
 
-app.post("/produtos/:id",(req,res)=>{
-    db.run(`INSERT INTO tabela(nome) VALUES(?)`,[req.params.id])
+app.get("/jogos", (req, res) => {
+    db.all(`SELECT * FROM jogos `, [], (err, rows) => {
+        res.json(rows)
+    })
+})
+
+app.post("/jogo/:id", (req, res) => {
+
+    const {nome,img,valor,descricao,classificacao} = req.body
+
+   db.run(`INSERT INTO jogos(nome,img,valor,descricao,classificacao) VALUES(?,?,?,?,?)`,[nome,img,valor,descricao,classificacao])
 })
 
 
-app.listen(8080,()=>{
+app.post("/userc/:id",(req,res)=>{
+
+    const {nome,email,senha} = req.body
+
+    db.run(`INSERT INTO login(nome,email,senha) VALUES(?,?,?)`,[nome,email,senha])
+
+})
+
+app.get("userl",(req,res)=>{
+    db.all(`SELECT * FROM login`,[],(err,rows)=>{
+        res.json(rows)
+    })
+
+})
+
+
+app.listen(8080, () => {
     console.log("Servidor aberto na porta 8080")
 })
