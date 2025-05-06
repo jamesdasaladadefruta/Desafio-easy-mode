c041553409e7d507226fcf1176facbf082ea7594
FlavioUser222 <flavinhoth12@gmail.com>
2025-05-06 14:22:34 -0300

    hello

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ import { dirname } from "path"
 import { fileURLToPath } from "url"
 
 
-const app = express()
+const app = express() 
+
+
+
+
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded())
